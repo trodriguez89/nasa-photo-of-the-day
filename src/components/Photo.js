7902c10bcbd82511d9bf353c66699db48fd43716
trodriguez89 <trodriguez89@gmail.com>
2019-11-06 15:39:14 -0800

    stretch wip

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from "react";
 import PhotoCard from "./PhotoCard";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 export default function Photo () {
     const [photoImg, setPhotoImg] = useState([])
-    const [date, setDate] = useState("");
+    const [startDate, setStartDate] = useState("2019-11-06");
 
     const img_temp = {
         width: "35%"
@@ -12,7 +16,7 @@ export default function Photo () {
 
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=1ppfJ03viLPpZ8HNDhAI8P2zB7CwW33GpjxoCx8U`)
+        axios.get(`https://api.nasa.gov/planetary/apod?date=${startDate}&api_key=1ppfJ03viLPpZ8HNDhAI8P2zB7CwW33GpjxoCx8U`)
         .then(response => {
             console.log(response)
             setPhotoImg(response.data);
@@ -20,11 +24,11 @@ export default function Photo () {
         .catch(error => {
             console.log("Oops, your request was lost in space!", error)
         })
-    },[date])
+    },[startDate])
 
     return (
     <div>
-        <h2>Date: {photoImg.date} <form>Enter a Date as YYYY-MM-DD: <input/><input date = {date}type="Submit"/> </form></h2>
+        <h2>Date: {photoImg.date} Pick A Date </h2>
         
         <PhotoCard arrayInfo = {photoImg}/>
         {/* <img style = {img_temp} src = {photoImg.hdrul} alt = "APOD IMG"/> */}
@@ -34,3 +38,5 @@ export default function Photo () {
 }
 
 {/* <button onClick={() => setDate(date - 1)}>View Previous Day's Photo!</button> */}
+
+{/* <DatePicker dateFormat = "yyyy-MM-dd" selected = {startDate} onChange ={() => setStartDate(startDate)}/> */}
